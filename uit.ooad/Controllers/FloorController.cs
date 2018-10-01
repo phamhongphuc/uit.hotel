528cc472705f01c0b2de6bec5aa08e13f7467552
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-10-01 08:50:36 +0700

    Add Floor Model, Controller, Bussiness, DataAccess

---
+++ uit.ooad/Controllers/FloorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using uit.ooad.Businesses;
+using uit.ooad.Models;
+
+namespace uit.ooad.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class FloorController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<string>> PostApiFloor([FromBody] Floor floor)
+        {
+            var savedFloor = await FloorBusiness.Add(floor);
+            return Ok(new
+            {
+                floor = savedFloor
+            });
+        }
+    }
+}
---
+++ uit.ooad/DataAccesses/FloorDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using uit.ooad.Models;
+
+namespace uit.ooad.DataAccesses
+{
+    public class FloorDataAccess : RealmDatabase
+    {
+        internal static async Task<Floor> Add(Floor floor)
+        {
+            await Database.WriteAsync(realm => floor = realm.Add(floor));
+            return floor;
+        }
+    }
+}
