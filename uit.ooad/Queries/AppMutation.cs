23cae39c740ec35b5510f5fa07376b8bbf011cb5
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-10-24 09:35:05 +0700

    Add GraphQL & GraphQL.Authorization

---
+++ uit.ooad/Queries/AppMutation.cs
@@ -0,0 +1,26 @@
+﻿using GraphQL.Types;
+using uit.ooad.Businesses;
+using uit.ooad.Models;
+using uit.ooad.ObjectTypes;
+
+namespace uit.ooad.Queries
+{
+    public class AppMutation : ObjectGraphType
+    {
+        public AppMutation()
+        {
+            Field<FloorType>(
+                "floor",
+                "Return a floor",
+                new QueryArguments(
+                    new QueryArgument<NonNullGraphType<StringGraphType>> {Name = "name"},
+                    new QueryArgument<NonNullGraphType<IntGraphType>> {Name = "id"}
+                ),
+                context => FloorBusiness.Add(new Floor
+                {
+                    Id = context.GetArgument<int>("id"),
+                    Name = context.GetArgument<string>("name")
+                }));
+        }
+    }
+}
--- uit.ooad/Startup.cs
+++ uit.ooad/Startup.cs
@@ -1,11 +1,23 @@
-﻿using System.Text;
+﻿using GraphQL;
+using GraphQL.Authorization;
+using GraphQL.DataLoader;
+using GraphQL.Http;
+using GraphQL.Types;
+using GraphQL.Validation;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using uit.ooad.ObjectTypes;
+using uit.ooad.Queries;
+using uit.ooad.Schemas;
 
 namespace uit.ooad
 {
@@ -15,17 +27,17 @@ namespace uit.ooad
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Auth
             services
                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-               .AddJwtBearer(jwtconfig =>
+               .AddJwtBearer(jwtBearerOptions =>
                 {
-                    jwtconfig.TokenValidationParameters = new TokenValidationParameters
+                    jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuer = true,
                         ValidateAudience = true,
@@ -35,12 +47,41 @@ namespace uit.ooad
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:key"]))
                     };
                 });
+
+            // GraphQL
+            services.AddSingleton<IDependencyResolver>(s => new FuncDependencyResolver(s.GetRequiredService));
+            services.AddSingleton<IDocumentExecuter, DocumentExecuter>();
+            services.AddSingleton<IDocumentWriter, DocumentWriter>();
+
+            services.AddSingleton<AppQuery>();
+            services.AddSingleton<AppMutation>();
+
+            services.AddSingleton<FloorType>();
+
+            services.AddSingleton<ISchema, AppSchema>();
+
+            // GraphQL.DataLoader
+            services.AddSingleton<IDataLoaderContextAccessor, DataLoaderContextAccessor>();
+            services.AddSingleton<DataLoaderDocumentListener>();
+
+            // GraphQL.Auth
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.TryAddSingleton<IAuthorizationEvaluator, AuthorizationEvaluator>();
+            services.AddTransient<IValidationRule, AuthorizationValidationRule>();
+            services.TryAddSingleton(s =>
+            {
+                var authSettings = new AuthorizationSettings();
+                authSettings.AddPolicy("AdminPolicy", _ => _.RequireClaim(ClaimTypes.Role, "Admin"));
+                return authSettings;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.ApplicationServices.GetServices<IValidationRule>();
 
             app.UseCors(x => x
                             .AllowAnyOrigin()
