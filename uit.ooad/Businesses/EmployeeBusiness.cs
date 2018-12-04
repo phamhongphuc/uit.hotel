7c50f043628a2d5a73f0d6ffc57155d99dc81f9e
thaotram <sawa.kisei@gmail.com>
2018-12-04 21:56:12 +0700

    Create BUS & DA: Employee

---
+++ uit.ooad/Businesses/EmployeeBusiness.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using uit.ooad.DataAccesses;
+using uit.ooad.Models;
+
+namespace uit.ooad.Businesses
+{
+    public class EmployeeBusiness
+    {
+        public static Task<Employee> Add(Employee Employee) => EmployeeDataAccess.Add(Employee);
+        public static Employee Get(string EmployeeId) => EmployeeDataAccess.Get(EmployeeId);
+        public static IEnumerable<Employee> Get() => EmployeeDataAccess.Get();
+    }
+}
---
+++ uit.ooad/DataAccesses/EmployeeDataAccess.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using uit.ooad.Models;
+
+namespace uit.ooad.DataAccesses
+{
+    public class EmployeeDataAccess : RealmDatabase
+    {
+        public static async Task<Employee> Add(Employee Employee)
+        {
+            await Database.WriteAsync(realm => Employee = realm.Add(Employee));
+            return Employee;
+        }
+
+        public static Employee Get(string EmployeeId) => Database.Find<Employee>(EmployeeId);
+
+        public static IEnumerable<Employee> Get() => Database.All<Employee>();
+    }
+}
