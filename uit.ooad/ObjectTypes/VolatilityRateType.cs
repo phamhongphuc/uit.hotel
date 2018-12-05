4aa6fd97261e9ee6e228f83098948a68b3c24789
thaotram <sawa.kisei@gmail.com>
2018-12-05 15:49:09 +0700

    Create ObjectGraphType: RateType, VolatilityRateType

---
+++ uit.ooad/ObjectTypes/VolatilityRateType.cs
@@ -0,0 +1,33 @@
+using GraphQL.Types;
+using uit.ooad.Models;
+
+namespace uit.ooad.ObjectTypes
+{
+    public class VolatilityRateType : ObjectGraphType<VolatilityRate>
+    {
+        public VolatilityRateType()
+        {
+            Name = "VolatilityRate";
+            Description = "Giá biến động của một loại phòng";
+
+            Field(x => x.Id).Description("Id của giá");
+            Field(x => x.DayRate).Description("Giá ngày");
+            Field(x => x.NightRate).Description("Giá đêm");
+            Field(x => x.WeekRate).Description("Giá tuần");
+            Field(x => x.MonthRate).Description("Giá tháng");
+            Field(x => x.LateCheckOutFee).Description("Phí check-out muộn");
+            Field(x => x.EarlyCheckInFee).Description("Phí check-out sớm");
+            Field(x => x.EffectiveStartDate).Description("Ngày giá bắt đầu có hiệu lực");
+            Field(x => x.EffectiveEndDate).Description("Ngày giá hết hiệu lực");
+            Field(x => x.EffectiveOnMonday).Description("Giá có hiệu lực vào ngày Thứ 2");
+            Field(x => x.EffectiveOnTuesday).Description("Giá có hiệu lực vào ngày Thứ 3");
+            Field(x => x.EffectiveOnWednesday).Description("Giá có hiệu lực vào ngày Thứ 4");
+            Field(x => x.EffectiveOnThursday).Description("Giá có hiệu lực vào ngày Thứ 5");
+            Field(x => x.EffectiveOnFriday).Description("Giá có hiệu lực vào ngày Thứ 6");
+            Field(x => x.EffectiveOnSaturday).Description("Giá có hiệu lực vào ngày Thứ 7");
+            Field(x => x.EffectiveOnSunday).Description("Giá có hiệu lực vào ngày Chủ Nhật");
+            Field(x => x.CreateDate).Description("Ngày tạo giá");
+            // Field(x => x.RoomType).Description("Thuộc loại phòng");
+        }
+    }
+}
