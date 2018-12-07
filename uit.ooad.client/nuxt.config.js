0ae381bfa9145c45e75dc77b378d6d0139f73168
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-12-07 16:13:29 +0700

    Format code

--- uit.ooad.client/nuxt.config.js
+++ uit.ooad.client/nuxt.config.js
@@ -3,8 +3,15 @@ module.exports = {
         title: 'uit.ooad.client',
         meta: [
             { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'Nuxt.js project',
+            },
         ],
         link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     },
