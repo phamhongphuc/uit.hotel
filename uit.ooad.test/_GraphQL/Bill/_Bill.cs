e4ab8b1f97cf8a609f716fc0c18626c4a35fbda4
thaotram <sawa.kisei@gmail.com>
2018-12-10 22:28:33 +0700

    Create Unit Test: Bill

---
+++ uit.ooad.test/_GraphQL/Bill/_Bill.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using uit.ooad.Businesses;
+using uit.ooad.Models;
+using uit.ooad.test.Helper;
+
+namespace uit.ooad.test._GraphQL._Bill
+{
+    [TestClass]
+    public class _Bill
+    {
+        [TestMethod]
+        public void Bills()
+        {
+            SchemaHelper.Execute(
+                @"/_GraphQL/Bill/query.bills.gql",
+                @"/_GraphQL/Bill/query.bills.schema.json"
+            );
+        }
+
+        [TestMethod]
+        public void CreateBill()
+        {
+            SchemaHelper.Execute(
+                @"/_GraphQL/Bill/mutation.createBill.gql",
+                @"/_GraphQL/Bill/mutation.createBill.schema.json",
+                @"/_GraphQL/Bill/mutation.createBill.variable.json",
+                p => p.PermissionCreateBill = true
+            );
+        }
+    }
+}
