88df6d300f76051352be746e9bcde9c1ec3b7e49
thaotram <sawa.kisei@gmail.com>
2018-12-12 22:48:54 +0700

    Add AuthenticationMutation: Login, ChangePassword

--- uit.ooad/Queries/Mutation/AuthenticationMutation.cs
+++ uit.ooad/Queries/Mutation/AuthenticationMutation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using GraphQL.Types;
 using uit.ooad.Businesses;
@@ -22,8 +23,27 @@ namespace uit.ooad.Queries.Mutation
                 context =>
                 {
                     var id = GetId<string>(context);
+                    var password = context.GetArgument<string>("password");
+                    EmployeeBusiness.CheckLogin(id, password);
 
                     return AuthenticationHelper.TokenBuilder(id);
+                }
+            );
+
+            Field<StringGraphType>(
+                "ChangePassword",
+                "Nhân viên tự đổi mật khẩu cho tài khoản của mình",
+                new QueryArguments(
+                    new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "password" },
+                    new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "newPassword" }
+                ),
+                context =>
+                {
+                    var id = AuthenticationHelper.GetEmployeeId(context);
+                    var password = context.GetArgument<string>("password");
+                    var newPassword = context.GetArgument<string>("newPassword");
+                    EmployeeBusiness.ChangePassword(id, password, newPassword);
+                    return "Đổi mật khẩu thành công";
                 }
             );
 
