5c2761512e60e3c4798afa312fec89e61f0c98e1
ShadowOfZ <nhutkhanhng@gmail.com>
2018-12-13 00:17:05 +0700

    Add CryptoHelper

---
+++ uit.ooad/Queries/Authentication/CryptoHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace uit.ooad.Queries.Authentication
+{
+    public static class CryptoHelper
+    {
+        private const int Keysize = 256;
+
+        private const int DerivationIterations = 1000;
+
+        private const string EncryptionKey = "PassOOAD";
+
+        private static string Encrypt(string plainText, string passPhrase)
+        {
+            var clearBytes = Encoding.Unicode.GetBytes(passPhrase);
+            using (var encryptor = Aes.Create())
+            {
+                var pdb = new Rfc2898DeriveBytes(plainText, new byte[]
+                {
+                    0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76
+                });
+
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+
+                    passPhrase = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+
+            return passPhrase;
+        }
+
+        private static string Decrypt(string cipherText, string EncryptionKey)
+        {
+            cipherText = cipherText.Replace(" ", "+");
+            var cipherBytes = Convert.FromBase64String(cipherText);
+            using (var encryptor = Aes.Create())
+            {
+                var pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[]
+                {
+                    0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76
+                });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+
+            return cipherText;
+        }
+
+        public static string Encrypt(string encryptString)
+        {
+            return Encrypt(EncryptionKey, encryptString);
+        }
+
+        public static string Decrypt(string cipherText)
+        {
+            return Decrypt(cipherText, EncryptionKey);
+        }
+    }
+}
