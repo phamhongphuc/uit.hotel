42b04d970699020ef0ada62f0739fe0f4e9d5a11
thaotram <sawa.kisei@gmail.com>
2018-12-12 22:45:51 +0700

    Update AuthenticationHelper: Extract method

--- uit.ooad/Queries/Authentication/AuthenticationHelper.cs
+++ uit.ooad/Queries/Authentication/AuthenticationHelper.cs
@@ -41,12 +41,9 @@ namespace uit.ooad.Queries.Authentication
 
         public static void HasPermission(ResolveFieldContext<object> context, Func<Position, bool> getPermission)
         {
-            if (!(context.UserContext is GraphQLUserContext userContext)) throw new Exception("Lỗi không xác định");
+            var employeeId = GetEmployeeId(context);
 
-            var name = userContext.User.FindFirst(u => u.Type == ClaimTypes.Name);
-            if (name == null) throw new Exception("Không tìm thấy tên đăng nhập");
-
-            var employee = EmployeeBusiness.Get(name.Value);
+            var employee = EmployeeBusiness.Get(employeeId);
             if (employee == null) throw new Exception("Không tim thấy tên đăng nhập trong hệ thống");
 
             var position = employee.Position;
@@ -54,6 +51,15 @@ namespace uit.ooad.Queries.Authentication
 
             var hasPermission = getPermission(position);
             if (!hasPermission) throw new Exception("Người dùng không có quyền thực hiện thao tác này");
+        }
+
+        public static string GetEmployeeId(ResolveFieldContext<object> context)
+        {
+            if (!(context.UserContext is GraphQLUserContext userContext)) throw new Exception("Lỗi không xác định");
+
+            var employeeId = userContext.User.FindFirst(u => u.Type == ClaimTypes.Name);
+            if (employeeId == null) throw new Exception("Không tìm thấy tên đăng nhập");
+            return employeeId.Value;
         }
     }
 }
