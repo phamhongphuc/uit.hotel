29da163724867ef60c3a5739241e161cbb6d3b5f
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-12-12 12:04:00 +0700

    Update _PatronKind.cs: Fix typo

--- uit.ooad.test/_GraphQL/PatronKind/_PatronKind.cs
+++ uit.ooad.test/_GraphQL/PatronKind/_PatronKind.cs
@@ -30,7 +30,7 @@ namespace uit.ooad.test._GraphQL._Patron
             SchemaHelper.Execute(
                 @"/_GraphQL/PatronKind/mutation.createPatronKind.gql",
                 @"/_GraphQL/PatronKind/mutation.createPatronKind.schema.json",
-                @"/_GraphQl/PatronKind/mutation.createPatronKind.variable.json",
+                @"/_GraphQL/PatronKind/mutation.createPatronKind.variable.json",
                 p => p.PermissionCreatePatronKind = true
             );
         }
