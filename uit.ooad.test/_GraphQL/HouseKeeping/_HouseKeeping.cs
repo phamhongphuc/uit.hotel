3b60c04ef76446009806321466bb5ac5ddafa954
Fijetso <henrynguyen02081997@gmail.com>
2018-12-12 23:22:18 +0700

    Add Unit Test: HouseKeepings

---
+++ uit.ooad.test/_GraphQL/HouseKeeping/_HouseKeeping.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using uit.ooad.Businesses;
+using uit.ooad.Models;
+using uit.ooad.test.Helper;
+
+namespace uit.ooad.test._GraphQL._HouseKeeping
+{
+    [TestClass]
+    public class _HouseKeeping
+    {
+        [TestMethod]
+        public void HouseKeepings()
+        {
+            SchemaHelper.Execute(
+                @"/_GraphQL/HouseKeeping/query.houseKeepings.gql",
+                @"/_GraphQL/HouseKeeping/query.houseKeepings.schema.json"
+            );
+        }
+        // [TestMethod]
+        // public void HouseKeeping()
+        // {
+        //     SchemaHelper.Execute(
+        //         @"/_GraphQL/HouseKeeping/query.houseKeeping.gql",
+        //         @"/_GraphQL/HouseKeeping/query.houseKeeping.schema.json"
+        //     );
+        // }
+        // [TestMethod]
+        // public void CreateHouseKeeping()
+        // {
+        //     SchemaHelper.Execute(
+        //         @"/_GraphQL/HouseKeeping/mutation.createHouseKeeping.gql",
+        //         @"/_GraphQL/HouseKeeping/mutation.createHouseKeeping.schema.json",
+        //         @"/_GraphQL/HouseKeeping/mutation.createHouseKeeping.variable.json",
+        //         p => p.PermissionCreateHouseKeeping = true
+        //     );
+        // }
+    }
+}
