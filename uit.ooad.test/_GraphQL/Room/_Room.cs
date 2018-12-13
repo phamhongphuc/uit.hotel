3e0b68e31c18b3a570f4bea16bc4c1cd35eaca09
Fijetso <henrynguyen02081997@gmail.com>
2018-12-13 15:39:57 +0700

    Fix Room

--- uit.ooad.test/_GraphQL/Room/_Room.cs
+++ uit.ooad.test/_GraphQL/Room/_Room.cs
@@ -21,7 +21,8 @@ namespace uit.ooad.test._GraphQL._Room
         {
             SchemaHelper.Execute(
                 @"/_GraphQL/Room/query.room.gql",
-                @"/_GraphQL/Room/query.room.schema.json"
+                @"/_GraphQL/Room/query.room.schema.json",
+                @"/_GraphQL/Room/query.room.variable.json"
             );
         }
     }
