77f8bc07ee42a2fe26983c6d27639967ad5b5343
thaotram <sawa.kisei@gmail.com>
2018-12-16 08:31:24 +0700

    Fix BillType

--- uit.ooad/ObjectTypes/BillType.cs
+++ uit.ooad/ObjectTypes/BillType.cs
@@ -20,7 +20,7 @@ namespace uit.ooad.ObjectTypes
                 resolve: context => context.Source.Patron,
                 description: "Thông tin khách hàng thanh toán hóa đơn");
 
-            Field<NonNullGraphType<EmployeeType>>(
+            Field<EmployeeType>(
                 nameof(Bill.Employee),
                 resolve: context => context.Source.Employee,
                 description: "Thông tin nhân viên nhận thanh toán hóa đơn");
