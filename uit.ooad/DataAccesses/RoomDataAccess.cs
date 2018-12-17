6005904f7132e5bdb60740b62171b693fb2d5f72
thaotram <sawa.kisei@gmail.com>
2018-12-17 22:07:23 +0700

    Auto Increase Id: Room

--- uit.ooad/DataAccesses/RoomDataAccess.cs
+++ uit.ooad/DataAccesses/RoomDataAccess.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using uit.ooad.Models;
 
@@ -8,7 +9,12 @@ namespace uit.ooad.DataAccesses
     {
         public static async Task<Room> Add(Room room)
         {
-            await Database.WriteAsync(realm => room = realm.Add(room));
+            await Database.WriteAsync(realm =>
+            {
+                room.Id = Get().Count() == 0 ? 1 : Get().Max(f => f.Id) + 1;
+
+                room = realm.Add(room);
+            });
             return room;
         }
 
