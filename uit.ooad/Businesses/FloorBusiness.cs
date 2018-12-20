5a1920dff286f090aa836496be85c59c5a2a1bc8
thaotram <sawa.kisei@gmail.com>
2018-12-20 10:53:16 +0700

    Fix Floor: Update, Delete, SetIsActive

--- uit.ooad/Businesses/FloorBusiness.cs
+++ uit.ooad/Businesses/FloorBusiness.cs
@@ -12,26 +12,26 @@ namespace uit.ooad.Businesses
         public static Task<Floor> Add(Floor floor) => FloorDataAccess.Add(floor);
         public static Task<Floor> Update(Floor floor)
         {
-            var floorInDatabase = FloorDataAccess.Get(floor.Id);
+            var floorInDatabase = Get(floor.Id);
             if (floorInDatabase != null && floorInDatabase.Rooms.Count() > 0)
                 throw new Exception("Tầng đã có phòng. Không thể cập nhật!");
-            return FloorDataAccess.Update(floor);
+            return FloorDataAccess.Update(floorInDatabase, floor);
         }
         public static void Delete(int floorId)
         {
             var floorInDatabase = Get(floorId);
             if (floorInDatabase == null)
-                throw new Exception("Tầng có Id=" + floorId + " không hợp lệ!");
+                throw new Exception("Tầng có Id: " + floorId + " không hợp lệ!");
             if (floorInDatabase.Rooms.Count() > 0)
                 throw new Exception("Tầng đã có phòng. Không thể xóa!");
             FloorDataAccess.Delete(floorInDatabase);
         }
         public static void SetIsActive(int floorId, bool isActive)
         {
-            var floorInDatabase = FloorDataAccess.Get(floorId);
+            var floorInDatabase = Get(floorId);
             if (floorInDatabase == null)
-                throw new Exception("Tầng có Id=" + floorId + " không hợp lệ!");
-            FloorDataAccess.SetIsActive(floorId, isActive);
+                throw new Exception("Tầng có Id: " + floorId + " không hợp lệ!");
+            FloorDataAccess.SetIsActive(floorInDatabase, isActive);
         }
         public static Floor Get(int floorId) => FloorDataAccess.Get(floorId);
         public static IEnumerable<Floor> Get() => FloorDataAccess.Get();
