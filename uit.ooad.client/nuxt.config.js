c122957c9439d7dfc4aa931ae9db95316a90f6f3
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-12-21 19:41:42 +0700

    Update nuxt config

--- uit.ooad.client/nuxt.config.js
+++ uit.ooad.client/nuxt.config.js
@@ -1,3 +1,7 @@
+const autoprefixer = require('autoprefixer');
+
+const LOCALHOST = `http://localhost:3000`;
+
 module.exports = {
     head: {
         title: 'uit.ooad.client',
@@ -13,13 +17,43 @@ module.exports = {
                 content: 'Nuxt.js project',
             },
         ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+        link: [
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css?family=Pacifico',
+            },
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        ],
     },
     server: {
         port: 8080,
     },
-    css: [{ src: 'bootstrap/scss/bootstrap.scss', lang: 'scss' }],
+    env: {},
+    css: [{ src: '~/assets/scss/main.scss', lang: 'scss' }],
     loading: { color: '#3B8070' },
+    modules: [
+        '@nuxtjs/apollo',
+        ['bootstrap-vue/nuxt', { css: false }],
+        // 'nuxt-sass-resources-loader',
+        // [
+        // 'assets/scss/before/_before.scss',
+        // 'bootstrap/scss/_functions.scss',
+        // 'bootstrap/scss/_variables.scss',
+        // 'bootstrap/scss/_mixins.scss',
+        // 'assets/scss/after/_after.scss',
+        // ],
+    ],
+    plugins: [
+        // { src: '~/plugins/global', ssr: false }
+    ],
+    apollo: {
+        clientConfigs: {
+            default: {
+                httpEndpoint: `${process.env.HTTP_ENDPOINT ||
+                    LOCALHOST}/api/graphql`,
+            },
+        },
+    },
     build: {
         extend(config, { isDev, isClient }) {
             if (isDev && isClient) {
@@ -30,14 +64,22 @@ module.exports = {
                     exclude: /(node_modules)/,
                 });
             }
+
+            const vueLoader = config.module.rules.find(
+                rule => rule.loader === 'vue-loader',
+            );
+
+            vueLoader.use = [
+                { loader: vueLoader.loader, options: vueLoader.options },
+                {
+                    loader: 'vue-import-loader',
+                    options: { allowPath: 'client' },
+                },
+            ];
+
+            delete vueLoader.options;
+            delete vueLoader.loader;
         },
-    },
-    modules: ['@nuxtjs/apollo'],
-    apollo: {
-        clientConfigs: {
-            default: {
-                httpEndpoint: 'http://localhost:3000/api/graphql',
-            },
-        },
+        postcss: [autoprefixer()],
     },
 };
