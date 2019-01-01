05838472fb1891c2f8a5360472a7db7d1b86995b
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2019-01-01 10:20:10 +0700

    Update mixins/button.ts: Add more props

--- uit.ooad.client/components/mixins/button.ts
+++ uit.ooad.client/components/mixins/button.ts
@@ -45,20 +45,99 @@ export class LinkProps extends Vue {
     to: [string, Record<string, any>];
 }
 
+// Copy and modify from bootstrap-vue/es/components/button/button.js
 @Mixin
-export class ButtonIconProps extends Vue {
+export class ButtonProps extends Vue {
+    @Prop({ default: false })
+    block: boolean;
+
+    @Prop({ default: false })
+    disabled: boolean;
+
+    @Prop({ default: null })
+    size: string;
+
+    @Prop({ default: null })
+    variant: string;
+
+    @Prop({ default: 'button' })
+    type: string;
+
+    @Prop({ default: null })
+    pressed: boolean;
+}
+
+@Mixin
+export class IconProps extends Vue {
+    @Prop({ default: null, validator: icon => icon.length === 1 })
+    icon: string;
+}
+
+// Valid supported input types
+var TYPES = [
+    'text',
+    'password',
+    'email',
+    'number',
+    'url',
+    'tel',
+    'search',
+    'range',
+    'color',
+    'date',
+    'time',
+    'datetime',
+    'datetime-local',
+    'month',
+    'week',
+];
+
+@Mixin
+export class InputProps extends Vue {
+    @Prop({ default: null })
+    value: any;
+
+    @Prop({
+        default: 'text',
+        validator(type) {
+            return TYPES.indexOf(type) !== -1;
+        },
+    })
+    type: string;
+
+    @Prop({ default: false })
+    ariaInvalid: [boolean, string];
+
+    @Prop({ default: false })
+    readonly: boolean;
+
+    @Prop({ default: false })
+    plaintext: boolean;
+
+    @Prop({ default: null })
+    autocomplete: string;
+
+    @Prop({ default: null })
+    placeholder: string;
+
+    @Prop()
+    formatter: Function;
+
+    @Prop({ default: false })
+    lazyFormatter: boolean;
+}
+
+@Mixin
+export class TextProps extends Vue {
     @Prop({ default: '' })
     textClass: string;
-
-    @Prop({ default: '' })
-    icon: string;
 
     @Prop({ default: '' })
     text: string;
 }
 
 @Mixin
-export class ButtonImageProps extends Vue {
+export class ImageProps extends Vue {
     @Prop({ default: '' })
     image: string;
 
