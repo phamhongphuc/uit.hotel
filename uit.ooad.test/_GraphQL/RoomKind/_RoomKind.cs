9052e1c54cba08b4caae9add9c9919109aa08a7c
thaotram <sawa.kisei@gmail.com>
2019-01-06 11:29:39 +0700

    Update Unit Test: RoomKind

--- uit.ooad.test/_GraphQL/RoomKind/_RoomKind.cs
+++ uit.ooad.test/_GraphQL/RoomKind/_RoomKind.cs
@@ -28,7 +28,7 @@ namespace uit.ooad.test._GraphQL
         }
 
         [TestMethod]
-        public void Mutation_SetIsActiveRoomKind()
+        public void Mutation_DeleteRoomKind()
         {
             Database.WriteAsync(realm => realm.Add(new RoomKind
             {
@@ -40,15 +40,57 @@ namespace uit.ooad.test._GraphQL
             })).Wait();
 
             SchemaHelper.Execute(
-                @"/_GraphQL/RoomKind/mutation.setIsActiveRoomKind.gql",
-                @"/_GraphQL/RoomKind/mutation.setIsActiveRoomKind.schema.json",
-                new { id = 10, isActive = false },
+                @"/_GraphQL/RoomKind/mutation.deleteRoomKind.gql",
+                @"/_GraphQL/RoomKind/mutation.deleteRoomKind.schema.json",
+                new { id = 10 },
                 p => p.PermissionManageMap = true
             );
         }
 
         [TestMethod]
-        public void Query_RoomKind()
+        public void Mutation_DeleteRoomKind_InvalidHasRoom()
+        {
+            SchemaHelper.ExecuteAndExpectError(
+                "Loại phòng đã có phòng sử dụng, không thể cập nhật/xóa!",
+                @"/_GraphQL/RoomKind/mutation.deleteRoomKind.gql",
+                new { id = 1 },
+                p => p.PermissionManageMap = true
+            );
+        }
+
+        [TestMethod]
+        public void Mutation_DeleteRoomKind_InvalidId()
+        {
+            SchemaHelper.ExecuteAndExpectError(
+                "Loại phòng có Id: 100 không tồn tại",
+                @"/_GraphQL/RoomKind/mutation.deleteRoomKind.gql",
+                new { id = 100 },
+                p => p.PermissionManageMap = true
+            );
+        }
+
+        [TestMethod]
+        public void Mutation_DeleteRoomKind_Invalid_InActive()
+        {
+            Database.WriteAsync(realm => realm.Add(new RoomKind
+            {
+                Id = 11,
+                Name = "Tên loại phòng",
+                AmountOfPeople = 1,
+                NumberOfBeds = 1,
+                IsActive = true
+            })).Wait();
+
+            SchemaHelper.ExecuteAndExpectError(
+                "Loại phòng có Id: 11 đã ngừng cung cấp. Không thể cập nhật/xóa!",
+                @"/_GraphQL/RoomKind/mutation.deleteRoomKind.gql",
+                new { id = 11 },
+                p => p.PermissionManageMap = true
+            );
+        }
+
+        [TestMethod]
+        public void Mutation_SetIsActiveRoomKind()
         {
             Database.WriteAsync(realm => realm.Add(new RoomKind
             {
@@ -60,9 +102,138 @@ namespace uit.ooad.test._GraphQL
             })).Wait();
 
             SchemaHelper.Execute(
+                @"/_GraphQL/RoomKind/mutation.setIsActiveRoomKind.gql",
+                @"/_GraphQL/RoomKind/mutation.setIsActiveRoomKind.schema.json",
+                new { id = 20, isActive = false },
+                p => p.PermissionManageMap = true
+            );
+        }
+
+        [TestMethod]
+        public void Mutation_SetIsActiveRoomKind_InvalidId()
+        {
+            SchemaHelper.ExecuteAndExpectError(
+                "Loại phòng có Id: 100 không hợp lệ!",
+                @"/_GraphQL/RoomKind/mutation.setIsActiveRoomKind.gql",
+                new { id = 100, isActive = false },
+                p => p.PermissionManageMap = true
+            );
+        }
+
+        [TestMethod]
+        public void Mutation_UpdateRoomKind()
+        {
+            Database.WriteAsync(realm => realm.Add(new RoomKind
+            {
+                Id = 30,
+                Name = "Tên loại phòng",
+                AmountOfPeople = 1,
+                NumberOfBeds = 1,
+                IsActive = true
+            })).Wait();
+
+            SchemaHelper.Execute(
+                @"/_GraphQL/RoomKind/mutation.updateRoomKind.gql",
+                @"/_GraphQL/RoomKind/mutation.updateRoomKind.schema.json",
+                new
+                {
+                    input = new
+                    {
+                        id = 30,
+                        name = "Loại 2",
+                        numberOfBeds = 2,
+                        amountOfPeople = 2
+                    }
+                },
+                p => p.PermissionManageMap = true
+            );
+        }
+
+        [TestMethod]
+        public void Mutation_UpdateRoomKind_InvalidHasRoom()
+        {
+            SchemaHelper.ExecuteAndExpectError(
+                "Loại phòng đã có phòng sử dụng, không thể cập nhật/xóa!",
+                @"/_GraphQL/RoomKind/mutation.updateRoomKind.gql",
+                new
+                {
+                    input = new
+                    {
+                        id = 1,
+                        name = "Loại 2",
+                        numberOfBeds = 2,
+                        amountOfPeople = 2
+                    }
+                },
+                p => p.PermissionManageMap = true
+            );
+        }
+
+        [TestMethod]
+        public void Mutation_UpdateRoomKind_InvalidId()
+        {
+            SchemaHelper.ExecuteAndExpectError(
+                "Loại phòng có Id: 100 không tồn tại",
+                @"/_GraphQL/RoomKind/mutation.updateRoomKind.gql",
+                new
+                {
+                    input = new
+                    {
+                        id = 100,
+                        name = "Loại 2",
+                        numberOfBeds = 2,
+                        amountOfPeople = 2
+                    }
+                },
+                p => p.PermissionManageMap = true
+            );
+        }
+
+        [TestMethod]
+        public void Mutation_UpdateRoomKind_InvalidId_InActive()
+        {
+            Database.WriteAsync(realm => realm.Add(new RoomKind
+            {
+                Id = 33,
+                Name = "Tên loại phòng",
+                AmountOfPeople = 1,
+                NumberOfBeds = 1,
+                IsActive = false
+            })).Wait();
+
+            SchemaHelper.ExecuteAndExpectError(
+                "Loại phòng có Id: 33 đã ngừng cung cấp. Không thể cập nhật/xóa!",
+                @"/_GraphQL/RoomKind/mutation.updateRoomKind.gql",
+                new
+                {
+                    input = new
+                    {
+                        id = 33,
+                        name = "Loại 2",
+                        numberOfBeds = 2,
+                        amountOfPeople = 2
+                    }
+                },
+                p => p.PermissionManageMap = true
+            );
+        }
+
+        [TestMethod]
+        public void Query_RoomKind()
+        {
+            Database.WriteAsync(realm => realm.Add(new RoomKind
+            {
+                Id = 40,
+                Name = "Tên loại phòng",
+                AmountOfPeople = 1,
+                NumberOfBeds = 1,
+                IsActive = true
+            })).Wait();
+
+            SchemaHelper.Execute(
                 @"/_GraphQL/RoomKind/query.roomKind.gql",
                 @"/_GraphQL/RoomKind/query.roomKind.schema.json",
-                new { id = 20 },
+                new { id = 40 },
                 p => p.PermissionGetMap = true
             );
         }
--- uit.ooad/Businesses/RoomKindBusiness.cs
+++ uit.ooad/Businesses/RoomKindBusiness.cs
@@ -27,7 +27,7 @@ namespace uit.ooad.Businesses
         {
             var roomKindInDatabase = Get(roomKindId);
             if (roomKindInDatabase == null)
-                throw new Exception("Tầng có Id: " + roomKindId + " không hợp lệ!");
+                throw new Exception("Loại phòng có Id: " + roomKindId + " không hợp lệ!");
             RoomKindDataAccess.SetIsActive(roomKindInDatabase, isActive);
         }
 
