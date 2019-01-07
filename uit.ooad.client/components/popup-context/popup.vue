82112cc8294cc3ceb3100d0821f3ecfbe40c8f0b
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2019-01-07 07:13:51 +0700

    Fix bug on Base components

--- uit.ooad.client/components/popup-context/popup.vue
+++ uit.ooad.client/components/popup-context/popup.vue
@@ -27,10 +27,10 @@ export default class extends Vue {
     show: boolean = false;
     data: any = null;
 
-    @Prop({ required: true })
+    @Prop({ required: true, type: String })
     title: string;
 
-    @Prop({ default: false })
+    @Prop({ default: false, type: Boolean })
     noData: boolean;
 
     close() {
