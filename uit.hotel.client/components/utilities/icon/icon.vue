5537289a895c9620c00d05ab18ef08c79f732bcd
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2019-04-17 23:17:04 +0700

    Create component icon
    Add resolveJsonModule in tsconfig.json

---
+++ uit.hotel.client/components/utilities/icon/icon.vue
@@ -0,0 +1,23 @@
+<template>
+    <span class="icon">{{ icon }}</span>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+import selection from './selection.json';
+
+@Component
+export default class extends Vue {
+    icon: string = '123';
+
+    mounted() {
+        const a = selection.icons.reduce((obj, current) => {
+            obj[current.properties.name] = String.fromCharCode(
+                current.properties.code,
+            );
+            return obj;
+        }, {});
+        // eslint-disable-next-line no-console
+        console.log(a);
+    }
+}
+</script>
