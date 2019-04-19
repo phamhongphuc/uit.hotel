e1b5b5541e838c835d5a4c7e67a837c779c6d435
phamhongphuc <phamhongphuc.info@gmail.com>
2019-04-19 18:08:40 +0700

    Update Icon Component

--- uit.hotel.client/components/bootstrap/b-input.vue
+++ uit.hotel.client/components/bootstrap/b-input.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="b-input" :class="{ 'has-icon': icon !== null }">
-        <div v-if="icon" class="icon">{{ icon }}</div>
+        <!-- <div v-if="icon" class="icon">{{ icon }}</div> -->
+        <icon- v-if="icon" :i="icon" />
         <b-input
             ref="input"
             :value="value"
--- uit.hotel.client/components/utilities/icon/icon.vue
+++ uit.hotel.client/components/utilities/icon/icon.vue
@@ -2,22 +2,35 @@
     <span class="icon">{{ icon }}</span>
 </template>
 <script lang="ts">
-import { Vue, Component } from 'nuxt-property-decorator';
+import { Vue, Component, Prop } from 'nuxt-property-decorator';
 import selection from './selection.json';
 
-@Component
-export default class extends Vue {
-    icon: string = '123';
+const map = selection.icons.reduce((obj, current) => {
+    obj[current.properties.name] = String.fromCharCode(current.properties.code);
+    return obj;
+}, {});
 
-    mounted() {
-        const a = selection.icons.reduce((obj, current) => {
-            obj[current.properties.name] = String.fromCharCode(
-                current.properties.code,
-            );
-            return obj;
-        }, {});
-        // eslint-disable-next-line no-console
-        console.log(a);
+@Component({
+    name: 'icon-',
+})
+export default class extends Vue {
+    @Prop({ required: true, type: String })
+    i?: string;
+
+    get icon(): string {
+        if (!this.i) return '';
+        if (this.i.length === 1 && this.i.charCodeAt(0) > 50000) {
+            // eslint-disable-next-line no-console
+            console.log(`%c [${this.i}]`, `color: #`);
+            return this.i;
+        }
+        const character = map[this.i];
+        if (character === undefined) {
+            // eslint-disable-next-line no-console
+            console.error(`Không tìm thấy icon name [${this.i}] trong map`);
+            return '';
+        }
+        return character;
     }
 }
 </script>
