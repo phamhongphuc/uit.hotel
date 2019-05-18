7efff36013dd4302036a2f83a5be631c6910e327
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2019-05-18 07:45:07 +0700

    Update PermissionMixin: Use Union

--- uit.hotel.client/components/mixins/permission.ts
+++ uit.hotel.client/components/mixins/permission.ts
@@ -1,38 +1,39 @@
+import { Position, UserLogin } from '~/graphql/types';
 import { Vue, Component, namespace, Prop } from 'nuxt-property-decorator';
-import { UserLogin } from '~/graphql/types';
 
-export enum PermissionEnum {
-    /** Quyền thao tác dọn phòng */
-    Cleaning,
-    /** Quyền lấy thông tin các chứng từ (hóa đơn, phiếu thu) */
-    GetAccountingVoucher,
-    /** Quyền tra cứu lịch sử dọn phòng */
-    GetHouseKeeping,
-    /** Quyền lấy thông tin tầng, phòng */
-    GetMap,
-    /** Quyền lấy thông tin khách hàng */
-    GetPatron,
-    /** Quyền lấy thông tin giá cơ bản và giá biến động */
-    GetRate,
-    /** Quyền lấy thông tin dịch vụ */
-    GetService,
-    /** Quyền quản lý thông tin nhân viên */
-    ManageEmployee,
-    /** Quyền quản lý thuê phòng */
-    ManageHiringRoom,
-    /** Quyền quản lý khách hàng */
-    ManagePatron,
-    /** Quyền quản lý loại khách hàng */
-    ManagePatronKind,
-    /** Quyền quản lý chức vụ */
-    ManagePosition,
-    /** Quyền quản lý giá cơ bản và giá biến động */
-    ManageRate,
-    /** Quyền quản lý dịch vụ */
-    ManageService,
-    /** Quyền chỉnh sửa sơ đồ */
-    ManageMap,
-}
+type PermissionType = Pick<
+    Position,
+    Exclude<
+        keyof Position,
+        | 'countActiveEmployees'
+        | 'countEmployees'
+        | 'countInActiveEmployees'
+        | 'employees'
+        | 'id'
+        | 'isActive'
+        | 'name'
+    >
+>;
+
+const PermissionInstance: PermissionType = {
+    permissionCleaning: true,
+    permissionGetAccountingVoucher: true,
+    permissionGetHouseKeeping: true,
+    permissionGetMap: true,
+    permissionGetPatron: true,
+    permissionGetRate: true,
+    permissionGetService: true,
+    permissionManageEmployee: true,
+    permissionManageHiringRoom: true,
+    permissionManagePatron: true,
+    permissionManagePatronKind: true,
+    permissionManagePosition: true,
+    permissionManageRate: true,
+    permissionManageService: true,
+    permissionManageMap: true,
+};
+
+type PermissionUnion = keyof PermissionType;
 
 @Component
 export class PermissionMixin extends Vue {
@@ -41,24 +42,20 @@ export class PermissionMixin extends Vue {
 
     @Prop({
         default: true,
-        validator(permission: boolean | string[] | PermissionEnum[]) {
-            if (typeof permission === 'boolean') return true;
-            else if (
-                typeof permission === 'object' &&
-                Array.isArray(permission)
-            ) {
-                return permission.every((p: string | PermissionEnum) => {
-                    const condition = p in PermissionEnum;
+        validator(value: boolean | string[]) {
+            if (typeof value === 'boolean') return true;
+            else if (typeof value === 'object' && Array.isArray(value)) {
+                return value.every((key: string) => {
+                    const condition =
+                        key in PermissionInstance ||
+                        'permission' + key in PermissionInstance;
                     if (!condition) {
-                        const permissionMessage =
-                            typeof p === 'string'
-                                ? `String: ${p}`
-                                : `Number: ${p} (${PermissionEnum[p]})`;
-                        // eslint-disable-next-line no-console
-                        console.warn(
-                            `${permissionMessage} is not a is not a valid prop for permission.\n`,
-                            `Valid value is`,
-                            { enum: PermissionEnum },
+                        console.error(
+                            `[Permission warn]: String "${key}" is not a is not a valid prop for permission.\n`,
+                            `Expect key to be one of these types:`,
+                            Object.keys(PermissionInstance).map(k =>
+                                k.replace(/^permission/, ''),
+                            ),
                         );
                     }
                     return condition;
@@ -67,7 +64,7 @@ export class PermissionMixin extends Vue {
             return false;
         },
     })
-    permission!: boolean | PermissionEnum[] | string[];
+    permission!: boolean | PermissionUnion[] | string[];
 
     get isShow() {
         const { permission, employee } = this;
@@ -75,10 +72,9 @@ export class PermissionMixin extends Vue {
         if (!employee) return true;
         else if (typeof permission === 'boolean') return permission;
         else if (permission === undefined) return false;
-        return permission.every((each: string | PermissionEnum) => {
-            let pString = each; // if p is string
-            if (typeof each === 'number') pString = PermissionEnum[each];
-            return employee.position['permission' + pString];
+        return permission.every((each: string | PermissionUnion) => {
+            const key = each; // if p is string
+            return employee.position['permission' + key];
         });
     }
 }
