428def5b5226bbe0d609eba4fed9239321554840
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2019-05-20 09:30:53 +0700

    Remove Vuex module: `view` & Remove return type

--- uit.hotel.client/store/index.ts
+++ uit.hotel.client/store/index.ts
@@ -1,9 +1,7 @@
 import { StyleState } from './style';
 import { UserState } from './user';
-import { ViewState } from './view';
 
 export interface RootState {
     style: StyleState;
     user: UserState;
-    view: ViewState;
 }
--- uit.hotel.client/store/style.ts
+++ uit.hotel.client/store/style.ts
@@ -10,13 +10,13 @@ export const state = (): StyleState => ({
 });
 
 export const mutations: MutationTree<StyleState> = {
-    setBreakpoint(state, text): void {
+    setBreakpoint(state, text) {
         state.breakpoint = text;
     },
 };
 
 export const actions: ActionTree<StyleState, RootState> = {
-    updateBreakpoint({ state, commit }): void {
+    updateBreakpoint({ state, commit }) {
         const w = window.innerWidth;
         let breakpoint = 'none';
         if (w < 576) breakpoint = 'xs';
