12e7afe1427ff785311a5379b96216560e1b32ae
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2019-05-20 20:52:25 +0700

    Update block-flex

--- uit.hotel.client/components/utilities/block-flex.vue
+++ uit.hotel.client/components/utilities/block-flex.vue
@@ -3,3 +3,9 @@
         <div class="d-flex"><slot /></div>
     </div>
 </template>
+<style lang="scss">
+.row-like {
+    margin-left: $grid-gutter-width / -2;
+    margin-right: $grid-gutter-width / -2;
+}
+</style>
