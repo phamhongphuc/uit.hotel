6ea941fbc5c9a40c16ef49114a8243c8a492053a
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2019-05-20 09:23:21 +0700

    Update index.d.ts

--- uit.hotel.client/index.d.ts
+++ uit.hotel.client/index.d.ts
@@ -1,10 +1,15 @@
 import Vue, { ComponentOptions } from 'vue';
 import VueRouter from 'vue-router';
+import { ApolloHelpers } from '~/interfaces/ApolloHelpers';
+import { ApolloProvider } from '~/interfaces/ApolloProvider';
 
 declare module 'vuex' {
     interface Store<S> {
         app: ComponentOptions<Vue> & {
             router: VueRouter;
+            $apolloHelpers: ApolloHelpers;
+            $apolloProvider: ApolloProvider;
+            apolloProvider: ApolloProvider;
         };
         $router: VueRouter;
     }
