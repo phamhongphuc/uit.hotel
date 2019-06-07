10d796807a7d05b4ba42fb33ef6df7ab94c269dc
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2019-06-07 20:10:08 +0700

    Update .azure.yml & format code

--- uit.hotel.client/plugins/component.ts
+++ uit.hotel.client/plugins/component.ts
@@ -2,13 +2,11 @@ import Vue from 'vue';
 
 const requireComponent = require.context('../components', true, /.vue$/);
 
-requireComponent.keys().forEach(
-    (fileName): void => {
-        const componentConfig = requireComponent(fileName);
+requireComponent.keys().forEach((fileName): void => {
+    const componentConfig = requireComponent(fileName);
 
-        const regexr = /^.(\/[\w-]+)+\/([\w-]+).vue$/i;
-        const name = fileName.replace(regexr, '$2');
+    const regexr = /^.(\/[\w-]+)+\/([\w-]+).vue$/i;
+    const name = fileName.replace(regexr, '$2');
 
-        Vue.component(name + '-', componentConfig.default || componentConfig);
-    },
-);
+    Vue.component(name + '-', componentConfig.default || componentConfig);
+});
