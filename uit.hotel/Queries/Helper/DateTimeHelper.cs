7888c623a7ff28e8257959a725b6eb98036e8f23
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2019-11-18 21:10:31 +0700

    Update DateTimeHelper

--- uit.hotel/Queries/Helper/DateTimeHelper.cs
+++ uit.hotel/Queries/Helper/DateTimeHelper.cs
@@ -1,28 +1,62 @@
 using System;
-using System.Collections.Generic;
-using System.Globalization;
 
 namespace uit.hotel.Queries.Helper
 {
     public static class DateTimeHelper
     {
-        public static IEnumerable<DateTimeOffset> EachDay(DateTimeOffset from, DateTimeOffset to)
-        {
-            for (var date = from; date <= to; date = date.AddDays(1))
-                yield return date;
-        }
-
         public static bool IsTwoDateRangesOverlap(DateTimeOffset aStart, DateTimeOffset aEnd, DateTimeOffset bStart,
                                                   DateTimeOffset bEnd)
             => aStart < bEnd && bStart < aEnd;
 
-        public static DateTimeOffset GetDate(int dd, int mm, int yyyy)
+        public static DateTimeOffset Round(this DateTimeOffset dateTime)
         {
-            return DateTimeOffset.ParseExact(
-                string.Format("{0}/{1}/{2}", dd, mm, yyyy),
-                "dd/MM/yyyy",
-                CultureInfo.InvariantCulture
+            var updated = dateTime.AddMinutes(15);
+            var minute = (int)(dateTime.Minute / 30) * 30;
+
+            return new DateTimeOffset(
+                updated.Year,
+                updated.Month,
+                updated.Day,
+                updated.Hour,
+                minute,
+                0,
+                TimeSpan.Zero
             );
         }
+
+        public static float FloatHour(this DateTimeOffset dateTime)
+        {
+            dateTime = dateTime.Round();
+            return dateTime.Hour + (float)dateTime.Minute / 60;
+        }
+
+        public static DateTimeOffset AtHour(this DateTimeOffset dateTime, int hour)
+        {
+            return new DateTimeOffset(
+                dateTime.Year,
+                dateTime.Month,
+                dateTime.Day,
+                hour,
+                0,
+                0,
+                TimeSpan.Zero
+            );
+        }
+
+        public static TimeSpan Round(this TimeSpan dateTime)
+        {
+            var updated = dateTime.Add(new TimeSpan(0, 15, 0));
+            var minutes = (int)(dateTime.Minutes / 30) * 30;
+
+            return new TimeSpan(
+                updated.Days,
+                updated.Hours,
+                minutes,
+                0
+            );
+        }
+
+        public static double FloatHour(this TimeSpan timeSpan)
+            => timeSpan.Round().TotalMinutes / 60;
     }
 }
