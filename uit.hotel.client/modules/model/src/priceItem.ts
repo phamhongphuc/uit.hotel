b8c5e4bb74354b3af8a3386e2d9613abbfb5eeb2
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2019-12-08 23:42:03 +0700

    Component: Create Horizontal Timeline

---
+++ uit.hotel.client/modules/model/src/priceItem.ts
@@ -0,0 +1,48 @@
+import { duration, Duration } from 'moment';
+import { GetBookingDetails, PriceItemKindEnum } from '~/graphql/types';
+import { toMoney } from '~/utils';
+
+const kingMap: {
+    [key in PriceItemKindEnum]: string;
+} = {
+    [PriceItemKindEnum.Hour]: 'giờ',
+    [PriceItemKindEnum.Night]: 'đêm',
+    [PriceItemKindEnum.Day]: 'ngày',
+    [PriceItemKindEnum.Week]: 'tuần',
+    [PriceItemKindEnum.Month]: 'tháng',
+};
+
+const getAmountMap = (
+    time: Duration,
+): { [key in PriceItemKindEnum]: () => number } => ({
+    [PriceItemKindEnum.Hour]: () => time.asHours(),
+    [PriceItemKindEnum.Night]: () => 1,
+    [PriceItemKindEnum.Day]: () => time.add(2, 'hour').asDays(),
+    [PriceItemKindEnum.Week]: () => time.add(2, 'hour').asWeeks(),
+    [PriceItemKindEnum.Month]: () => time.add(2, 'hour').asMonths(),
+});
+
+const getUnitPriceMap = (
+    price: GetBookingDetails.Price,
+): { [key in PriceItemKindEnum]: () => number } => ({
+    [PriceItemKindEnum.Hour]: () => price.hourPrice,
+    [PriceItemKindEnum.Night]: () => price.nightPrice,
+    [PriceItemKindEnum.Day]: () => price.dayPrice,
+    [PriceItemKindEnum.Week]: () => price.weekPrice,
+    [PriceItemKindEnum.Month]: () => price.monthPrice,
+});
+
+export function getPriceItemText(
+    booking: GetBookingDetails.Booking,
+    priceItem: GetBookingDetails.PriceItems,
+) {
+    const unit = kingMap[priceItem.kind];
+    const unitPrice = getUnitPriceMap(booking.price)[priceItem.kind]();
+    const number = parseFloat(
+        getAmountMap(duration(priceItem.timeSpan, 'second'))
+            [priceItem.kind]()
+            .toFixed(2),
+    );
+
+    return `${number} ${unit} ✕ ${toMoney(unitPrice)}`;
+}
