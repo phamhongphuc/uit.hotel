cb3ca60b7f96f24898d9a1abecb5e1195c17e4d8
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2019-12-27 08:35:46 +0700

    Receipt: Update Model & Type

--- uit.hotel/ObjectTypes/ReceiptType.cs
+++ uit.hotel/ObjectTypes/ReceiptType.cs
@@ -4,6 +4,24 @@ using uit.hotel.Queries.Base;
 
 namespace uit.hotel.ObjectTypes
 {
+    public class ReceiptKindEnumType : EnumerationGraphType<ReceiptKindEnum>
+    {
+        public ReceiptKindEnumType()
+        {
+            Name = nameof(ReceiptKindEnum);
+            Description = "Loại phiếu thu";
+        }
+    }
+
+    public class ReceiptStatusEnumType : EnumerationGraphType<ReceiptStatusEnum>
+    {
+        public ReceiptStatusEnumType()
+        {
+            Name = nameof(ReceiptStatusEnum);
+            Description = "Loại phiếu thu";
+        }
+    }
+
     public class ReceiptType : ObjectGraphType<Receipt>
     {
         public ReceiptType()
@@ -14,16 +32,29 @@ namespace uit.hotel.ObjectTypes
             Field(x => x.Id).Description("Id của phiếu thu");
             Field(x => x.Money).Description("Số tiền đã thu");
             Field(x => x.Time).Description("Thời gian tạo phiếu thu");
+            Field(x => x.PayUrl).Description("Đường dẫn thanh toán");
+            Field(x => x.StatusText).Description("Trạng thái của phiếu thu");
+            Field(x => x.OrderInfo).Description("Thông tin thanh toán");
+            Field(x => x.ExtraData).Description("Thông tin thêm");
 
+            Field<NonNullGraphType<ReceiptStatusEnumType>>(
+                nameof(Receipt.Status),
+                "Trạng thái thanh toán"
+            );
+            Field<NonNullGraphType<ReceiptKindEnumType>>(
+                nameof(Receipt.Kind),
+                "Loại thanh toán"
+            );
             Field<NonNullGraphType<BillType>>(
                 nameof(Receipt.Bill),
                 "Phiếu thu thuộc hóa đơn nào",
-                resolve: context => context.Source.Bill);
-
+                resolve: context => context.Source.Bill
+            );
             Field<NonNullGraphType<EmployeeType>>(
                 nameof(Receipt.Employee),
                 "Nhân viên tạo phiếu thu",
-                resolve: context => context.Source.Employee);
+                resolve: context => context.Source.Employee
+            );
         }
     }
 
@@ -37,6 +68,21 @@ namespace uit.hotel.ObjectTypes
                 nameof(Receipt.Bill),
                 "Thuộc hóa đơn"
             );
+            Field<NonNullGraphType<ReceiptKindEnumType>>(
+                nameof(Receipt.Kind),
+                "Loại thanh toán"
+            );
+        }
+    }
+
+    public class ReceiptIdInput : InputType<Receipt>
+    {
+        public ReceiptIdInput()
+        {
+            Name = _Id;
+            Description = "Input cho thông tin một phiếu thu";
+
+            Field(x => x.Id).Description("Id của phiếu thu");
         }
     }
 }
